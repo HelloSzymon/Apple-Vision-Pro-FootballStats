2775d30330af163b83ceda3e9a9e08448c4199d6
Szymon Wnuk <101000022+HelloSzymon@users.noreply.github.com>
2024-04-01 22:11:17 +0200

    Initial Commit

---
+++ FootballStats/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FootballStats
+//
+//  Created by Szymon Wnuk on 01/04/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ FootballStats/FootballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballStatsApp.swift
+//  FootballStats
+//
+//  Created by Szymon Wnuk on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FootballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
